76cb59542a5e3bbdcea389a984ad34e3698dd529
Gino Ascencio <gino.francisco@hotmail.com>
2022-09-10 22:12:30 -0500

    init configs for config server

---
+++ src/main/java/com/exams/microservices/appexamconfigserver/AppExamConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.exams.microservices.appexamconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class AppExamConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppExamConfigServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/exams/microservices/appexamconfigserver/AppExamConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exams.microservices.appexamconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExamConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
